87bd66565a40efdb80694bf6ad2ac76b31359fc7
akshay pandey <kumarakshaypandey@gmail.com>
2020-12-22 14:32:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
-
+app.get("/topRankings", async (req, res) => {
+    const { limit, offset } = req.query;
+    const lim = limit === undefined ? 20 : isNaN(limit) ? 20 : Number(limit);
+    const off = offset === undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
+    res.send(data.slice(off,off+lim));
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
+
